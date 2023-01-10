c3881bbdcbd6beb269ea2cefb510a47b871ffc1b
Mustafa Aydogdu <mustafa.aydogdu@yahoo.com>
2023-01-10 12:14:15 +0400

    Update SAP View Generator.sql

--- SAP View Generator.sql
+++ SAP View Generator.sql
@@ -79,7 +79,7 @@ return result;
 $$;
 
 CALL sp_generate_sap_views(
-        array_construct('BSEG', 'MARA,), -- list of tables
+        array_construct('BSEG', 'MARA'), -- list of tables
         'SAP_PLAYGROUND.RAW', --source schema to read SAP tables from
         'SAP_PLAYGROUND.RAW', -- target schema to create views
         'SAP_PLAYGROUND.RAW.DD03M', -- SAP Dictionary table DD03M
